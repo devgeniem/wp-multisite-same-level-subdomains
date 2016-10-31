463e61289763a2bb8d04e551fede3aa1e912df6b
Onni Hakala <onni@keksi.io>
2016-10-31 23:30:53 +0200

    Version bump

--- wp-multisite-same-level-subdomains.php
+++ wp-multisite-same-level-subdomains.php
@@ -7,7 +7,7 @@
  * Author URI: https://github.com/onnimonni
  * License: MIT
  * License URI: https://opensource.org/licenses/MIT
- * Version: 1.2
+ * Version: 2.1
  */
 
 namespace Geniem\Multisite;
